3319f8b71b8afd7b60757102fdc3db1ebeb92014
markbulk <markbulk@gmail.com>
2016-02-16 20:22:13 -0500

    updates after review

--- plot2.R
+++ plot2.R
@@ -1,14 +1,20 @@
 library(data.table)
 setwd('/Volumes/MacStorage/Coursera Data/exdata-data-NEI_data')
 
-## This first line will likely take a few seconds. Be patient!
-NEI <- data.table(readRDS("summarySCC_PM25.rds"))
-SCC <- data.table(readRDS("Source_Classification_Code.rds"))
-
-# Plot 2: Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (𝚏𝚒𝚙𝚜 == "𝟸𝟺𝟻𝟷𝟶") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
-dt.plot <- NEI[fips == "24510", list(Emissions = sum(Emissions)), by=year]
-png("plot2.png", width = 480, height = 480)
-barplot(dt.plot$Emissions/1e3, names.arg=dt.plot$year, 
-        ylab="Total Baltimore City Emissions (Thousands of Tons of PM2.5)", xlab = "Year")
-abline(h=0)
-dev.off()
+required_files <- c("Source_Classification_Code.rds", "summarySCC_PM25.rds")
+if(all(required_files %in% dir())) {
+    ## This first line will likely take a few seconds. Be patient!
+    NEI <- data.table(readRDS("summarySCC_PM25.rds"))
+    SCC <- data.table(readRDS("Source_Classification_Code.rds"))
+    
+    # Plot 2: Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (𝚏𝚒𝚙𝚜 == "𝟸𝟺𝟻𝟷𝟶") from 1999 
+    #         to 2008? Use the base plotting system to make a plot answering this question.
+    dt.plot <- NEI[fips == "24510", list(Emissions = sum(Emissions)), by=year]
+    png("plot2.png", width = 480, height = 480)
+    barplot(dt.plot$Emissions/1e3, names.arg=dt.plot$year, 
+            ylab="Total Baltimore City EmissionsPM2.5  (Thousands of Tons of PM2.5= "Year")
+    abline(h=0)
+    dev.off()
+} else {
+    print("Required files not found in directory!")
+}
--- plot4.R
+++ plot4.R
@@ -2,17 +2,23 @@ library(data.table)
 library(ggplot2)
 setwd('/Volumes/MacStorage/Coursera Data/exdata-data-NEI_data')
 
-## This first line will likely take a few seconds. Be patient!
-NEI <- data.table(readRDS("summarySCC_PM25.rds"))
-SCC <- data.table(readRDS("Source_Classification_Code.rds"))
-
-# Plot 4: Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
-dt.plot <- merge(NEI, SCC[grepl("^Fuel Comb -", EI.Sector) & grepl("- Coal$", EI.Sector)], by = "SCC")
-
-png("plot4.png", width = 480, height = 480)
-ggplot(data=dt.plot[, list(Emissions = sum(Emissions)), by=year],
-       mapping=aes(x=factor(year), y=Emissions/1e6)) +
-    geom_bar(stat="identity", fill="steelblue") + geom_hline(yintercept = 0) +
-    scale_x_discrete(name = "Year") + 
-    scale_y_continuous(name = "Coal Combustion-Related PM2.5\n(Millions of Tons)")
-dev.off()
+required_files <- c("Source_Classification_Code.rds", "summarySCC_PM25.rds")
+if(all(required_files %in% dir())) {
+    ## This first line will likely take a few seconds. Be patient!
+    NEI <- data.table(readRDS("summarySCC_PM25.rds"))
+    SCC <- data.table(readRDS("Source_Classification_Code.rds"))
+    
+    # Plot 4: Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
+    dt.plot <- merge(NEI, SCC[grepl("^Fuel Comb -", EI.Sector) & grepl("- Coal$", EI.Sector)], by = "SCC")
+    
+    png("plot4.png", width = 480, height = 480)
+    ggplot(data=dt.plot[, list(Emissions = sum(Emissions)), by=year],
+           mapping=aes(x=factor(year), y=Emissions/1e3)) +
+        geom_bar(stat="identity", fill="steelblue") + geom_hline(yintercept = 0) +
+        scale_x_discrete(name = "Year") + 
+        scale_y_continuous(name = "U.S. Coal Combustion-Related PM2.5\n(Thousands of Tons)") +
+        geom_text(mapping=aes(y=Emissions/1e3+30, label=paste0(round(Emissions/1e3,1))))
+    dev.off()
+} else {
+    print("Required files not found in directory!")
+}
